7d21f5570e1796a97737bd9de2f1b7a471ce3e1a
laurapras <83669900+laurapras@users.noreply.github.com>
2022-07-13 14:06:21 +0300

    masyvo skaiciavimai

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
+'use strict'; 
 console.log('laba sekmingo darbo');
  
 //  debuger 
+const mainArray = [2, 12, 33, -7, 28, 49, -17, 19, 42, -14, -10, 20];
+console.log(`masyvas ${mainArray}`);
+
+console.group('3. padauginti masyvo narius is 2');
+let arrCopy = mainArray.slice();
+
+const arrLen = arrCopy.length;
+
+for(let i = 0; i < arrLen; i++){
+arrCopy[i] = arrCopy[i] * 2;
+
+}
+console.log(arrCopy);
+console.groupEnd();
+{
+//  pakelti masyvo narius kvadratu
+const mainArray = [2, 12, 33, -7, 28, 49, -17, 19, 42, -14, -10, 20];
+console.log(`masyvas ${mainArray}`);
+
+console.group('3. padauginti masyvo narius is 2');
+let arrCopy = mainArray.slice();
+
+const arrLen = arrCopy.length;
+
+for(let i = 0; i < arrLen; i++){
+arrCopy[i] = arrCopy[i] ** 2;
+
+}
+console.log(arrCopy);
+console.groupEnd();
+}
